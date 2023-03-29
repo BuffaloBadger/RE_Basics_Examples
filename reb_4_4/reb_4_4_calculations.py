86fda7dc623257da7555edf02d8ad47f48a539f4
Carl Lund <BuffaloBadger@gmail.com>
2023-03-28 20:23:51 -0400

    revised reb_4_4

--- reb_4_4/reb_4_4_calculations.py
+++ reb_4_4/reb_4_4_calculations.py
@@ -7,34 +7,19 @@ import math
 # given or known
 R = 8.314E-3 # kJ/mol/K
 
-# read the data file
+# read the data file and convert the temperature to K
 df = pd.read_csv('./reb_4_4/reb_4_4_data.csv')
 df.columns = ['i','T','k']
-T = df['T'].to_numpy()
+T = df['T'].to_numpy() + 273.15
 k = df['k'].to_numpy()
 
-# create adjusted input vector, eqn 4
-x = T + 273.15
-
-# create experimental response vector, eqn 5
-y_expt = k
-
-# response model function
-def calc_y(T_K,log_k0,E):
-    return 10**log_k0*math.e**(-E/R/T_K) # eqn 3
-
-# guess the values of the parameters
-guess = np.array([0.,20.])
-
-# fit the function to the data
-use_relative_errors = False
-beta, beta_ci, r_squared = reb.fitNLSR(guess,x,y_expt,calc_y,
-    use_relative_errors)
+# fit the Arrhenius expression to the data
+beta, beta_ci, r_squared = reb.fit_Arrhenius_model(k,T,R)
 
 # extract the results
-k0 = 10**beta[0] # eqn 6
-k0_lower = 10**beta_ci[0,0] # eqn 6
-k0_upper = 10**beta_ci[0,1] #eqn 6
+k0 = beta[0]
+k0_lower = beta_ci[0,0]
+k0_upper = beta_ci[0,1]
 E = beta[1]
 E_lower = beta_ci[1,0]
 E_upper = beta_ci[1,1]
@@ -47,45 +32,23 @@ print(f'R-squared: {r_squared:.3g}')
 print(' ')
 
 # save the results to a .csv file
-data = [['k0', f'{k0:.3g}', 'L/mol/min'],
-    ['k0_lower_limit', f'{k0_lower:.3g}', 'L/mol/min'],
-    ['k0_upper_limit', f'{k0_upper:.3g}', 'L/mol/min'],
-    ['E', f'{E:.3g}', 'kJ/mol'],
-    ['E_lower_limit', f'{E_lower:.3g}', 'kJ/mol'],
-    ['E_upper_limit', f'{E_upper:.3g}', 'kJ/mol'],
+data = [['k0', f'{k0:.3g}', 'L mol^-1^ min^-1^'],
+    ['k0_lower_limit', f'{k0_lower:.3g}', 'L mol^-1^ min^-1^'],
+    ['k0_upper_limit', f'{k0_upper:.3g}', 'L mol^-1^ min^-1^'],
+    ['E', f'{E:.3g}', 'kJ mol^-1^'],
+    ['E_lower_limit', f'{E_lower:.3g}', 'kJ mol^-1^'],
+    ['E_upper_limit', f'{E_upper:.3g}', 'kJ mol^-1^'],
     ['R_squared', f'{r_squared:.3g}', '']]
 result = pd.DataFrame(data, columns=['item','value','units'])
-if use_relative_errors:
-    result.to_csv("./reb_4_4/reb_4_4_Python_results_rel.csv", index=False)
-else:
-    result.to_csv("./reb_4_4/reb_4_4_Python_results_abs.csv", index=False)
+result.to_csv("./reb_4_4/reb_4_4_Python_results.csv", index=False)
 
-# generate the parity plot
-y_pred = calc_y(x,beta[0],beta[1]) # eqn 7
-parity_range = np.array([0.9*np.min(y_expt), 1.1*np.max(y_expt)])
-plt.figure(1)
-plt.loglog(y_expt,y_pred,color='r',marker='o', ls='none', label='Actual Model')
-plt.loglog(parity_range,parity_range,color='k', label='Perfect Model')
-plt.xlabel('Measured k (L mol$^{-1}$ min$^{-1}$)')
-plt.ylabel('Predicted k (L mol$^{-1}$ min$^{-1}$)')
-plt.legend()
+y_pred = k0*np.exp(-E/R/T)
+# generate a model plot
+plt.figure()
+plt.semilogy(1/T,k,color='r',marker='o', ls='none')
+plt.semilogy(1/T,y_pred,color='k')
+plt.xlabel('T$^{-1}$ (K$^{-1}$)')
+plt.ylabel('k (L mol$^{-1}$ min$^{-1}$)')
 # save and show the figure
-if use_relative_errors:
-    plt.savefig('reb_4_4/reb_4_4_Python_parity_plot_rel.png')
-else:
-    plt.savefig('reb_4_4/reb_4_4_Python_parity_plot_abs.png')
-plt.show()
-
-# generate the residuals plot
-residual = y_expt - y_pred # eqn 8
-plt.figure(2)
-plt.plot(x,residual,color='r',marker='o',ls='none')
-plt.axhline(y=0, color = 'k')
-plt.xlabel('T (K)')
-plt.ylabel('Residual (L mol$^{-1}$ min$^{-1}$)')
-# save and show the figure
-if use_relative_errors:
-    plt.savefig('reb_4_4/reb_4_4_Python_residual_plot_rel.png')
-else:
-    plt.savefig('reb_4_4/reb_4_4_Python_residual_plot_abs.png')
+plt.savefig('reb_4_4/reb_4_4_Python_Arrhenius_plot.png')
 plt.show()
