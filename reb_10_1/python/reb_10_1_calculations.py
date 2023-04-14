87a5c8a694c7562ef30fe0d8ae93650ac1a38f2e
Carl Lund <BuffaloBadger@gmail.com>
2023-04-14 13:38:01 -0400

    added first draft of reb_10_1

---
+++ reb_10_1/python/reb_10_1_calculations.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import numpy as np
+import math
+import rebutils as reb
+import matplotlib.pyplot as plt
+
+# Given and known constants
+P = 1.0 # atm
+D = 1.0 # cm
+L = 10.0 # cm
+T = 1500. # K
+mol_per_cc_at_stp = 1.0E-3/22.4
+
+# path for saving files
+filepath = './reb_10_1/python/'
+
+# Read the experimental data into a dataframe
+df = pd.read_csv("reb_10_1/reb_10_1_data.csv")
+        # columns: A_in, Y_in, Z_in, fA
+
+# Extract the data as arrays
+Vdot_Ain = df['A_in'].to_numpy()
+Vdot_Yin = df['Y_in'].to_numpy()
+Vdot_Zin = df['Z_in'].to_numpy()
+fA = df['fA'].to_numpy()
+
+# combine the adjusted inputs into a matrix
+adjusted_inputs = np.transpose(np.array([Vdot_Ain, Vdot_Yin, Vdot_Zin]))
+
+# Allocate storage for the responses
+resp = np.ones(len(Vdot_Ain)) * -1
+
+# Define the response function
+def response_function(inputs, par):
+    # extract the rate coefficient
+    k_test = 10.0**par
+
+    # loop through the data points
+    for i, input in enumerate(inputs):
+        # define the reactor model
+        def reactor_model(z,nDot):
+            nDot_A = nDot[0]
+            nDot_Y = nDot[1]
+            nDot_Z = nDot[2]
+            PA = nDot_A/(nDot_A + nDot_Y + nDot_Z)*P
+            r = k_test*PA
+            ddz = np.array([-r, r, r]) * math.pi *D**2/4
+            return ddz
+
+        # Solve the reactor model equations
+        z0 = 0.0
+        n0 = np.array([input[0], input[1], input[2]])*mol_per_cc_at_stp
+        f_var = 0
+        f_val = L
+        soln = reb.solveIVODEs(z0, n0, f_var, f_val, reactor_model)
+        if not(soln.success):
+            print("A solution was NOT obtained:")
+            print(soln.message)
+        # Calculate the response
+        nA = soln.y[0,-1]
+        nAin = input[0]*mol_per_cc_at_stp
+        resp[i] = 100*(nAin - nA)/nAin
+    return resp
+
+# Estimate the kinetics parameters
+par_guess = [-2.0]
+beta, beta_ci, r_squared = reb.fitNLSR(par_guess, adjusted_inputs, fA, 
+        response_function, False)
+
+# extract the results
+k = 10**beta[0]
+k_ll = 10**beta_ci[0,0]
+k_ul = 10**beta_ci[0,1]
+
+# report the results
+print(' ')
+print(f'k: {k:.3g} mol/cc/min/atm, 95% CI ['\
+      + f'{k_ll:.3g}, {k_ul:.3g}]')
+print(f'R-squared: {r_squared:.3g}')
+print(' ')
+
+# save the results to a .csv file
+data = [['k', f'{k:.3g}', 'mol cm^-3^ min^-1^ atm^-1^'],
+    ['k_ll', f'{k_ll:.3g}', 'mol cm^-3^ min^-1^ atm^-1^'],
+    ['k_ul', f'{k_ul:.3g}', 'mol cm^-3^ min^-1^ atm^-1^'],
+    ['R_squared', f'{r_squared:.3g}', '']]
+result = pd.DataFrame(data, columns=['item','value','units'])
+result.to_csv(filepath + "reb_10_1_results.csv", index=False)
+
+# calculate the model-predicted responses
+y_model = response_function(adjusted_inputs,beta[0])
+
+# calculate the residuals
+residuals = fA - y_model
+
+# create a parity plot
+plt.figure(1) 
+plt.plot(fA, y_model, color = 'r', marker='o', ls='')
+plt.plot([np.min(fA), np.max(fA)], [np.min(fA), np.max(fA)], color = 'k')
+plt.xlabel("experimental response (%)")
+plt.ylabel("model-predicted response (%)")
+
+# save and show the parity plot
+filename = filepath + 'reb_10_1_parity.png'
+plt.savefig(filename)
+plt.show()
+
+# create a residuals plot for the inlet A flow rate
+plt.figure(2) 
+plt.plot(Vdot_Ain, residuals, color = 'r', marker='o', ls='')
+plt.axhline(y=0, color = 'k')
+plt.xlabel("Reagent A Inlet Volumetric Flow Rate (sccm)")
+plt.ylabel("Residual (%)")
+
+# save and show the residuals plot for the reaction time
+filename = filepath + 'reb_10_1_residuals_vs_VFR_A.png'
+plt.savefig(filename)
+plt.show()
+
+# create a residuals plot for the inlet Y flow rate
+plt.figure(2) 
+plt.plot(Vdot_Yin, residuals, color = 'r', marker='o', ls='')
+plt.axhline(y=0, color = 'k')
+plt.xlabel("Reagent Y Inlet Volumetric Flow Rate (sccm)")
+plt.ylabel("Residual (%)")
+
+# save and show the residuals plot for the reaction time
+filename = filepath + 'reb_10_1_residuals_vs_VFR_Y.png'
+plt.savefig(filename)
+plt.show()
+
+# create a residuals plot for the inlet Z flow rate
+plt.figure(2) 
+plt.plot(Vdot_Zin, residuals, color = 'r', marker='o', ls='')
+plt.axhline(y=0, color = 'k')
+plt.xlabel("Reagent Z Inlet Volumetric Flow Rate (sccm)")
+plt.ylabel("Residual (%)")
+
+# save and show the residuals plot for the reaction time
+filename = filepath + 'reb_10_1_residuals_vs_VFR_Z.png'
+plt.savefig(filename)
+plt.show()
---
+++ reb_10_1/reb_10_1_pfr_data_generator.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+import rebutils as reb
+import math
+import random
+
+# Constant inputs
+D = 1.0 # cm
+L = 10.0 # cm
+P = 1.0 # atm
+T = 1500.0 # K
+R = 8.314E-3 # kJ/mol/K
+mol_per_cc_stp = 1.0/1000.0/22.4 # mol/cc
+Rpv = 82.06 # cc atm/mol/K
+
+# kinetics parameters
+k = 1.5E-3 # mol cm-3 min-1 atm-1
+
+# Define rate expression
+def rate(P_A):
+    return k*P_A
+
+# Adjusted inputs
+VFR_Ain = np.array([30, 60, 90, 120, 150, 180, 210, 240, 270, 300])
+VFR_Yin = np.array([0, 50, 100, 150])
+VFR_Zin = np.array([0, 50, 100, 150])
+
+# Create empty dataframe for the results
+df = pd.DataFrame(columns=["A_in", "Y_in", "Z_in", "fA"])
+
+# Calculate the responses
+for VdotAin in VFR_Ain:
+    nAin = VdotAin * mol_per_cc_stp
+    for VdotYin in VFR_Yin:
+        nYin = VdotYin * mol_per_cc_stp
+        for VdotZin in VFR_Zin:
+            nZin = VdotZin * mol_per_cc_stp
+            
+            # Define the mole balances
+            def mole_balances(z,n):
+                n_total = np.sum(n)
+                PA = n[0]/n_total*P
+                PY = n[1]/n_total*P
+                PZ = n[2]/n_total*P
+                r = rate(PA)
+                ddz = np.array([-r, r, r]) *math.pi*D**2/4
+                return ddz
+            
+            # Solve the mole balances
+            z0 = 0
+            n0 = np.array([nAin, nYin, nZin])
+            f_var = 0
+            f_val = L
+            soln = reb.solveIVODEs(z0, n0, f_var, f_val, mole_balances)
+
+            # calculate the response
+            nA = soln.y[0,-1]
+            fA = (nAin - nA)/nAin*100.0
+
+            # add +/- 0.01 random "error"
+            random_error = (2*random.random() - 1.0)*2.0
+            fA += random_error
+
+            # round to 3 decimal places
+            fA = round(fA,1)
+
+            # append the result to the dataframe
+            df.loc[len(df.index)] = [VdotAin, VdotYin, VdotZin, fA]
+
+# display the results
+print("\n")
+print(df)
+
+# save the results
+filename = "reb_10_1/reb_10_1_data.csv"
+print("\nSaving results to " + filename + "\n")
+df.to_csv(filename,index=False)
