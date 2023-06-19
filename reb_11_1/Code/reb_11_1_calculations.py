cfa05e1693227e81e8bb0e5b4da4dec06c0268bf
Carl Lund <BuffaloBadger@gmail.com>
2023-06-19 14:56:41 -0400

    revised example 11-1

--- reb_11_1/Code/reb_11_1_calculations.py
+++ reb_11_1/Code/reb_11_1_calculations.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import math
+import response_function as rf
 import rebutils as reb
 import matplotlib.pyplot as plt
 
@@ -28,45 +28,10 @@ fA = df['fA'].to_numpy()
 # combine the adjusted inputs into a matrix
 adjusted_inputs = np.transpose(np.array([Vdot_Ain, Vdot_Yin, Vdot_Zin]))
 
-# Allocate storage for the responses
-resp = np.ones(len(Vdot_Ain)) * -1
-
-# Define the response function
-def response_function(inputs, par):
-    # extract the rate coefficient
-    k_test = 10.0**par
-
-    # loop through the data points
-    for i, input in enumerate(inputs):
-        # define the reactor model
-        def reactor_model(z,nDot):
-            nDot_A = nDot[0]
-            nDot_Y = nDot[1]
-            nDot_Z = nDot[2]
-            PA = nDot_A/(nDot_A + nDot_Y + nDot_Z)*P
-            r = k_test*PA
-            ddz = np.array([-r, r, r]) * math.pi *D**2/4
-            return ddz
-
-        # Solve the reactor model equations
-        z0 = 0.0
-        n0 = np.array([input[0], input[1], input[2]])*mol_per_cc_at_stp
-        f_var = 0
-        f_val = L
-        soln = reb.solveIVODEs(z0, n0, f_var, f_val, reactor_model)
-        if not(soln.success):
-            print("A solution was NOT obtained:")
-            print(soln.message)
-        # Calculate the response
-        nA = soln.y[0,-1]
-        nAin = input[0]*mol_per_cc_at_stp
-        resp[i] = 100*(nAin - nA)/nAin
-    return resp
-
 # Estimate the kinetics parameters
 par_guess = [-2.0]
 beta, beta_ci, r_squared = reb.fitNLSR(par_guess, adjusted_inputs, fA, 
-        response_function, False)
+        rf.response_function, False)
 
 # extract the results
 k = 10**beta[0]
@@ -89,7 +54,7 @@ result = pd.DataFrame(data, columns=['item','value','units'])
 result.to_csv(filepath1 + "reb_11_1_results.csv", index=False)
 
 # calculate the model-predicted responses
-y_model = response_function(adjusted_inputs,beta[0])
+y_model = rf.response_function(adjusted_inputs,beta[0])
 
 # calculate the residuals
 residuals = fA - y_model
