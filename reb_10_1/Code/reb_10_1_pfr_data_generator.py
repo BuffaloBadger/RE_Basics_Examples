4ada08b0f86491ef7ba5be463a91518f4e7efaef
Carl Lund <BuffaloBadger@gmail.com>
2023-08-07 14:11:27 -0400

    renumbering 10_1

--- reb_10_1/Code/reb_10_1_pfr_data_generator.py
+++ reb_10_1/Code/reb_10_1_pfr_data_generator.py
@@ -73,10 +73,10 @@ print("\n")
 print(df)
 
 # save the results
-filename = "reb_11_1/Data/reb_11_1_data.csv"
+filename = "reb_10_1/Data/reb_10_1_data.csv"
 print("\nSaving results to " + filename + "\n")
 df.to_csv(filename,index=False)
 
-filename = "../RE_Basics/Data/reb_11_1_data.csv"
+filename = "../RE_Basics/Data/reb_10_1_data.csv"
 print("\nSaving results to " + filename + "\n")
 df.to_csv(filename,index=False)
