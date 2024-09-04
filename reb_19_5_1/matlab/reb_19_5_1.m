705b1e624d6e7595e9956ee25fe0d3279facee62
Carl Lund <BuffaloBadger@gmail.com>
2024-09-04 15:58:47 -0400

    revised 19.5.1

--- reb_19_5_1/matlab/reb_19_5_1.m
+++ reb_19_5_1/matlab/reb_19_5_1.m
@@ -5,19 +5,24 @@ function reb_19_5_1()
     V = 1.0; % L
     R = 8.314E-3; % kJ /mol /K
 
-    % make k available to all functions
-    k_current = nan;
+    % make k0, E, and T globally available
+    k0_current = nan;
+    E_current = nan;
+    T_current = nan;
 
     % derivatives function
     function derivs = derivatives(~,dep)
         % get nA
         nA = dep(1);
 
+        % calculate the rate coefficient
+        k = k0_current*exp(-E_current/R/T_current);
+
         % calculate the concentration
         CA = nA/V;
 
         % calculate the rate
-        r = k_current*CA;
+        r = k*CA;
 
         % evaluate the derivatives
         dnAdt = -r*V;
@@ -28,7 +33,12 @@ function reb_19_5_1()
     end
 
     % BSTR model function
-    function [t, nA, nZ] = profiles(CA0,tf)
+    function [t, nA, nZ] = profiles(k0,E,T,CA0,tf)
+        % make rate expression parameters and adjusted inputs available
+        k0_current = k0;
+        E_current = E;
+        T_current = T;
+
         % initial values and stopping criterion
         ind_0 = 0.0;
         nA0 = CA0*V;
@@ -55,9 +65,10 @@ function reb_19_5_1()
     end
 
     % predicted responses function
-    function CAf_model = predicted_responses(log_k_guess, expt_inputs)
+    function CAf_model = predicted_responses(guesses, expt_inputs)
         % set the current value of k
-        k_current = 10^log_k_guess;
+        k0 = 10^guesses(1);
+        E = guesses(2);
 
         % get the number of experiments
         n_expt = size(expt_inputs,1);
@@ -68,9 +79,10 @@ function reb_19_5_1()
         % loop through the experiments
         for i = 1:n_expt
             % solve the BSTR design equations
+            T = expt_inputs(i,1);
             CA0 = expt_inputs(i,2);
             tf = expt_inputs(i,3);
-            [~, nA, ~] = profiles(CA0, tf);
+            [~, nA, ~] = profiles(k0,E,T,CA0,tf);
 
             % calculate the response
             CAf_model(i) = nA(end)/V;
@@ -80,124 +92,81 @@ function reb_19_5_1()
     % function that performs the calculations
     function perform_the_calculations()
         % read the experimental data
-        data_file = '../reb_19_5_1_data.csv';
-        data_table = readtable(data_file, 'VariableNamingRule'...
-            , 'preserve');
-        data = table2array(data_table(:,2:end));
+        data_table = readtable('../reb_19_5_1_data.csv'...
+            , 'VariableNamingRule', 'preserve');
+        adj_inputs = table2array(data_table(:,2:4));
+        adj_inputs(:,1) = adj_inputs(:,1) + 273.15;
+        CAf = table2array(data_table(:,5));
 
-        % get the temperatures of the data blocks
-        block_temperatures = unique(data(:,1));
-        n_blocks = size(block_temperatures,1);
+        % guess the log 10 of k0 and E
+        guess = [2.0; 20.0];
 
-        % create vectors to store the fitting results
-        k_fit = nan(n_blocks,1);
-        kll_fit = nan(n_blocks,1);
-        kul_fit = nan(n_blocks,1);
-        rsq_fit = nan(n_blocks,1);
+        % estimate the parameters
+        useRelErr = false;
+        [beta, betaCI, r_squared] = fit_to_SR_data(guess...
+            , adj_inputs, CAf, @predicted_responses, useRelErr);
 
-        % process the data blocks
-        for iBlock = 1:n_blocks
-            % create the block
-            idx = table2array(data_table(:,2) == block_temperatures(iBlock));
-            data_block = data(idx,:);
+        % extract the results
+        k0 = 10^beta(1);
+        k0ll = 10^betaCI(1,1);
+        k0ul = 10^betaCI(1,2);
+        E = beta(2);
+        Ell = betaCI(2,1);
+        Eul = betaCI(2,2);
 
-            % extract the adjusted inputs and response
-            adj_inputs = data_block(:,1:3);
-            CAf = data_block(:,4);
+        % calculate the predicted responses and experimental residuals
+        CAf_model = predicted_responses(beta, adj_inputs);
+        epsilon_expt = CAf - CAf_model;
 
-            % guess log 10 of k
-            par_guess = -2.0;
+        % create, show, and save a parity plot
+        figure
+        hold on
+        plot([min(CAf),max(CAf)], [min(CAf),max(CAf)],'r'...
+            ,'LineWidth',2)
+        plot(CAf, CAf_model,'ok','MarkerSize',10,'LineWidth',2)
+        hold off
+        set(gca, 'FontSize', 14);
+        xlabel('Experimental C_A (M)','FontSize', 14)
+        ylabel('Predicted C_A (M)','FontSize', 14)
+        saveas(gcf,'reb_19_5_1_parity.png')
 
-            % estimate log 10 of k
-            useRelErr = false;
-            [beta, betaCI, r_squared] = fit_to_SR_data(par_guess...
-                , adj_inputs, CAf, @predicted_responses, useRelErr);
+        % create show, and save residuals plots
+        figure
+        tf = adj_inputs(:,3);
+        plot(tf, epsilon_expt,'ok','MarkerSize',10,'LineWidth',2)
+        yline(0.0,'r','LineWidth',2)
+        set(gca, 'FontSize', 14);
+        xlabel('Elapsed Time (min)','FontSize', 14)
+        ylabel('Residual (M)','FontSize', 14)
+        saveas(gcf,'reb_19_5_1_residuals_tf.png')
 
-            % extract the results
-            k_fit(iBlock) = 10^beta;
-            kll_fit(iBlock) = 10^betaCI(1);
-            kul_fit(iBlock) = 10^betaCI(2);
-            rsq_fit(iBlock) = r_squared;
+        figure
+        CA0 = adj_inputs(:,2);
+        plot(CA0,epsilon_expt,'ok','MarkerSize',10,'LineWidth',2)
+        yline(0.0,'r','LineWidth',2)
+        set(gca, 'FontSize', 14);
+        xlabel('CA0 (M)','FontSize', 14)
+        ylabel('Residual (M)','FontSize', 14)
+        saveas(gcf,'reb_19_5_1_residuals_CA0.png')
 
-            % calculate the model-predicted response and the residuals
-            CAf_model = predicted_responses(beta, adj_inputs);
-            residual = CAf - CAf_model;
-            T_as_text = int2str(data_block(1,1));
-
-            % create, show, and save a parity plot
-            figure
-            hold on
-            plot([min(CAf),max(CAf)], [min(CAf),max(CAf)],'r'...
-                ,'LineWidth',2)
-            plot(CAf, CAf_model,'ok','MarkerSize',10,'LineWidth',2)
-            hold off
-            set(gca, 'FontSize', 14);
-            xlabel('Experimental C_A (M)','FontSize', 14)
-            ylabel('Predicted C_A (M)','FontSize', 14)
-            fname = strcat('reb_19_5_1_resp_fcn_parity_',T_as_text...
-                ,'.png');
-            saveas(gcf,fname)
-
-            % create show, and save residuals plots
-            figure
-            tf = adj_inputs(:,3);
-            plot(tf, residual,'ok','MarkerSize',10,'LineWidth',2)
-            yline(0.0,'r','LineWidth',2)
-            set(gca, 'FontSize', 14);
-            xlabel('Elapsed Time (min)','FontSize', 14)
-            ylabel('Residual (M)','FontSize', 14)
-            fname = strcat('reb_19_5_1_resp_fcn_residual_tf_'...
-                ,T_as_text,'.png');
-            saveas(gcf,fname)
-
-            figure
-            CA0 = adj_inputs(:,2);
-            plot(CA0, residual,'ok','MarkerSize',10,'LineWidth',2)
-            yline(0.0,'r','LineWidth',2)
-            set(gca, 'FontSize', 14);
-            xlabel('CA0 (M)','FontSize', 14)
-            ylabel('Residual (M)','FontSize', 14)
-            fname = strcat('reb_19_5_1_resp_fcn_residual_CA0_'...
-                ,T_as_text,'.png');
-            saveas(gcf,fname)
-            
-        end
-
-        % tabulate, show, and save the results
-        results_file ="reb_19_5_1_resp_fcn_params.csv";
-        results_table = table(block_temperatures, k_fit, kll_fit...
-            , kul_fit, rsq_fit);
-        results_table.Properties.VariableNames = ["T", "k", "k_ll"...
-            , "k_ul", "R_sq"];
-        disp(results_table)
-        writetable(results_table,results_file);
-
-        % fit the Arrhenius expression to the k vs. T data
-        T = block_temperatures + 273.15;
-        [k0, k0_ci, E, E_ci, r_squared] = Arrhenius_parameters(k_fit...
-            ,T,R);
+        figure
+        T = adj_inputs(:,1) - 273.15;
+        plot(T,epsilon_expt,'ok','MarkerSize',10,'LineWidth',2)
+        yline(0.0,'r','LineWidth',2)
+        set(gca, 'FontSize', 14);
+        xlabel('T (°C)','FontSize', 14)
+        ylabel('Residual (M)','FontSize', 14)
+        saveas(gcf,'reb_19_5_1_residuals_T.png')
 
         % tabulate, show, and save the results
         item = ["k0"; "k0_lower_limit"; "k0_upper_limit"; "E"; 
             "E_lower_limit"; "E_upper_limit"; "R_squared"];
-        value = [k0; k0_ci(1); k0_ci(2); E; E_ci(1); E_ci(2);
-             r_squared];
+        value = [k0; k0ll; k0ul; E; Ell; Eul; r_squared];
         units = ["min^-1^"; "min^-1^"; "min^-1^"; "kJ mol^-1^";
             "kJ mol^-1^"; "kJ mol^-1^"; ""];
-        Arrhenius_table = table(item, value, units);
-        disp(Arrhenius_table)
-        writetable(Arrhenius_table,'reb_19_5_1_Arrhenius_resp_fcn.csv')
-
-        % create, show and save an Arrhenius plot
-        k_pred = k0*exp(-E/R./T);
-        x = 1./T;
-        figure
-        semilogy(x, k_fit,'ok', x, k_pred,'r', 'MarkerSize' ,10 ...
-            ,'LineWidth',2)
-        set(gca, 'FontSize', 14);
-        xlabel('1/T (/K)','FontSize', 14)
-        ylabel('k (/min)','FontSize', 14)
-        saveas(gcf,'reb_19_5_1_Arrhenius_resp_fcn.png')
+        results_table = table(item, value, units);
+        disp(results_table)
+        writetable(results_table,'reb_19_5_1_results.csv')
     end
 
     % perform the calculations
